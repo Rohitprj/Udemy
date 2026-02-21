0abca760e8eed032cd5002c491c2439de3461600
Rohit kumar <rohitprj.1282@gmail.com>
2026-02-21 14:26:45 +0530

    some clean up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,14 +7,10 @@ export default function RootLayout() {
   const scheme = useColorScheme();
   return (
     <SafeAreaProvider>
-      {/* <StatusBar
-        style={scheme === "dark" ? "light" : "dark"}
-        backgroundColor={scheme === "dark" ? "#000" : "#fff"}
-      /> */}
       <StatusBar style={scheme === "dark" ? "dark" : "light"} />
       <Stack>
-        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="register" options={{ headerShown: false }} />
       </Stack>
     </SafeAreaProvider>
